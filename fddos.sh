756f6346b9185c09442c70b4934ef8ef4d6f85f9
Renzie2161 <78648295+Renzie2161@users.noreply.github.com>
2021-02-06 23:44:41 +0700

    Add files via upload

---
+++ fddos.sh
@@ -0,0 +1,455 @@
+toilet -f slant --gay ONLINE
+sleep 2
+toilet -f slant --gay DDOS
+sleep 2
+toilet -f slant --gay Online
+sleep 2
+
+echo "\33[33;1m[+]-------------------------[+]"
+echo "Coder : @mackenzie"
+echo "email : vhorsx@gmail.com"
+echo "Special thanks to : Ricko En"
+echo "\33[33;1m[+]-------------------------[+]"
+echo "(c) Copyright by Coldtopia"
+echo "\33[37;1m"
+sleep 2
+echo -n "Masukan IP ADRESS : "
+read ip;
+sleep 5
+echo "Found IP"
+
+echo -n "Masukan Port : "
+read port;
+sleep 5
+echo "Found port"
+
+echo -n "Masukan Pack (135/500) : "
+read pack;
+sleep 5
+
+echo "\33[32;1m"
+echo "IP : $ip , port : $port , pack : $pack"
+echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+if [ $pack = "500" ];
+then
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    toilet -f slant --gay DONE !
+fi
+
+if [ $pack = "135" ];
+then
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    echo "\33[32;1mSENDING 1 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 2 TO $ip PORT $port"
+    sleep 0.5
+    echo "SENDING 3 TO $ip PORT $port"
+    sleep 0.5
+    toilet -f slant --gay DONE !
+fi
